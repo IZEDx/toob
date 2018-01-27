3a47dcff7d093c218e65430a99c3233a6e5b8bb2
Niklas Kühtmann <admin@i-zed.net>
2018-01-27 22:43:18 +0100

    First usable UI

--- src/main.ts
+++ src/main.ts
@@ -29,45 +29,86 @@ import { saveToFile } from "./libs/utils";
 })();
 
 async function main() {
-    let ffmpeg: FFmpegWorker = new FFmpegWorker();
+    const ffmpeg: FFmpegWorker = new FFmpegWorker();
     console.log("Loading FFmpeg...");
-    await ffmpeg.waitReady();
-    console.log("FFmpeg ready!");
+
+    let filename = "";
+    let id = "";
+    let video: Uint8Array = <any>null;
 
     $("#app > .search > input").change(async ev => {
         const search: string = ""+$(ev.target).val();
+        console.log("Searching...");
+        $("#app .status").text("Searching...");
         try {
             const info = await getInfo(search);
+
             $("#app > .infoview > .title").text(info.title);
-            $("#app > .infoview > .controls > .btn").attr("disabled");
+            $("#app > .infoview").css("display", "grid");
+
+            $("#app .download").css("display", "inline-block");
+            $("#app .convert-mp3").css("display", "none");
+            $("#app .convert-mp4").css("display", "none");
+
+            filename = info.video_id;
+            id = info.video_id;
+
+            $("#app .status").text("Ready");
+            console.log("Found: " + info.title);
         } catch (err) {
-            alert(err);
-            $("#app > .infoview > .title").text("");
-            $("#app > .infoview > .controls > .btn").attr("disabled", "disabled");
+            $("#app > .infoview").css("display", "none");
+
+            console.log("Not found.")
+        }
+    });
+
+    $("#app .download").click(async () => {
+        console.log("Downloading: " + id);
+        $("#app .status").text("Downloading...");
+        $("#app .download").attr("disabled", "disabled");
+        try {
+            video = await downloadVideo(id);
+            $("#app .convert-mp3").css("display", "inline-block");
+            $("#app .convert-mp4").css("display", "inline-block");
+            $("#app .download").removeAttr("disabled");
+            $("#app .download").css("display", "none");
+            $("#app .status").text("Download successful");
+        } catch(err) {
+            console.error(err);
+            $("#app .download").removeAttr("disabled");
+            $("#app .status").text("Download failed");
         }
     });
 
     $("#app .convert-mp4").click(async () => {
-        const search: string = ""+$("#app > .search > input").val();
-        console.log(search);
         try {
-            const video = await downloadVideo(search);
-            saveToFile(video, "file.mp4");
+            $("#app .status").text("Saving as MP4...");
+            saveToFile(video, filename + ".mp4");
+            $("#app .status").text("Saved");
         } catch(err) {
-            alert(err);
+            console.error(err);
+            $("#app .status").text("Error saving as MP4");
         }
     });
 
     $("#app .convert-mp3").click(async () => {
-        const search: string = ""+$("#app > .search > input").val();
         try {
-            const video = await downloadVideo(search);
+            $("#app .status").text("Converting to MP3...");
             const audio = await ffmpeg.convertToMp3(video);
-            saveToFile(audio, "file.mp3");
+
+            $("#app .status").text("Saving as MP3...");
+            saveToFile(audio, filename + ".mp3");
+            $("#app .status").text("Saved");
         } catch(err) {
-            alert(err);
+            console.error(err);
+            $("#app .status").text("Error saving as MP3");
         }
     });
+
+    await ffmpeg.waitReady();
+    console.log("FFmpeg ready!");
+    $("#app > .search > input").removeAttr("disabled");
+    $("#app > .search > input").focus();
 
     /*
     let video: Uint8Array = <any>null;
