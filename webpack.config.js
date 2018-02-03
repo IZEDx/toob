641e194527554122fe1b034de934604876f739a5
Niklas Kühtmann <admin@i-zed.net>
2018-02-03 21:22:52 +0100

    forgot webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,5 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const AsyncModulePlugin = require('async-module-loader/plugin');
-
-const extractSass = new ExtractTextPlugin("style.css");
-const extractHtml = new ExtractTextPlugin("index.html");
 
 module.exports = {
     entry: './src/main.tsx',
@@ -20,30 +15,13 @@ module.exports = {
         publicPath: '/'
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.sass', '.html', '.js', '.json']
+        extensions: ['.ts', '.tsx', '.js', '.json']
     },
     module: {
         loaders: [
             {
                 test: /\.tsx?$/,
                 loader: 'ts-loader'
-            },
-            {
-                test: /\.sass$/,
-                use: extractSass.extract({
-                    use: [{
-                        loader: "css-loader"
-                    }, {
-                        loader: "resolve-url-loader"
-                    }, {
-                        loader: "sass-loader"
-                    }],
-                    fallback: "style-loader"
-                })
-            },
-            {
-                test: /\.html$/,
-                use: extractHtml.extract("html-loader")
             },
             { 
                 enforce: "pre", 
@@ -53,12 +31,11 @@ module.exports = {
         ]
     },
     plugins: [
-        extractSass, extractHtml,
         new CopyWebpackPlugin([
             { from: 'node_modules/ffmpeg.js/ffmpeg-worker-mp4.js' },
-            { from: 'assets' }
-        ]),
-        new AsyncModulePlugin()
+            { from: 'assets' },
+            { from: 'src/index.html' }
+        ])
     ],
     externals: {
     },
