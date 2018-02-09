42d9a71803b25aefbfb9387a17db93e420dde289
Niklas Kühtmann <admin@i-zed.net>
2018-02-09 20:46:49 +0100

    Removed commented out code

--- src/main.tsx
+++ src/main.tsx
@@ -27,92 +27,9 @@ async function main() {
     const app = (<App/>);
     
     FFmpegWorker.singleton();
-    
-    console.log("Loading FFmpeg...");
 
     render(app, document.getElementById("app"));
 
-    
-    /*
-    let filename = "";
-    let id = "";
-    let video: Uint8Array = null as any;
-
-    $("#app > .search > input").change(async ev => {
-        const search: string = ""+$(ev.target).val();
-        console.log("Searching...");
-        $("#app .status").text("Searching...");
-        try {
-            const info = await getInfo(search);
-
-            $("#app > .infoview > .title").text(info.title);
-            $("#app > .infoview").css("display", "grid");
-
-            $("#app .download").css("display", "inline-block");
-            $("#app .convert-mp3").css("display", "none");
-            $("#app .convert-mp4").css("display", "none");
-
-            filename = info.video_id;
-            id = info.video_id;
-
-            $("#app .status").text("Ready");
-            console.log("Found: " + info.title);
-        } catch (err) {
-            $("#app > .infoview").css("display", "none");
-
-            console.log("Not found.")
-        }
-    });
-
-    $("#app .download").click(async () => {
-        console.log("Downloading: " + id);
-        $("#app .status").text("Downloading...");
-        $("#app .download").attr("disabled", "disabled");
-        try {
-            video = await downloadVideo(id);
-            $("#app .convert-mp3").css("display", "inline-block");
-            $("#app .convert-mp4").css("display", "inline-block");
-            $("#app .download").removeAttr("disabled");
-            $("#app .download").css("display", "none");
-            $("#app .status").text("Download successful");
-        } catch(err) {
-            console.error(err);
-            $("#app .download").removeAttr("disabled");
-            $("#app .status").text("Download failed");
-        }
-    });
-
-    $("#app .convert-mp4").click(async () => {
-        try {
-            $("#app .status").text("Saving as MP4...");
-            saveToFile(video, filename + ".mp4");
-            $("#app .status").text("Saved");
-        } catch(err) {
-            console.error(err);
-            $("#app .status").text("Error saving as MP4");
-        }
-    });
-
-    $("#app .convert-mp3").click(async () => {
-        try {
-            $("#app .status").text("Converting to MP3...");
-            const audio = await ffmpeg.convertToMp3(video);
-
-            $("#app .status").text("Saving as MP3...");
-            saveToFile(audio, filename + ".mp3");
-            $("#app .status").text("Saved");
-        } catch(err) {
-            console.error(err);
-            $("#app .status").text("Error saving as MP3");
-        }
-    });
-
-    await ffmpeg.waitReady();
-    console.log("FFmpeg ready!");
-    $("#app > .search > input").removeAttr("disabled");
-    $("#app > .search > input").focus();
-
-    */
 }
 
 main();
