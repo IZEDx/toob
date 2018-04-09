ea4a585e6329b164c40920762691111cc9897d30
Niklas Kühtmann <admin@i-zed.net>
2018-04-09 17:30:31 +0200

    Updated production config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,13 +1,31 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.config.js');
-const MinifyPlugin = require("babel-minify-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = merge(common, {
     plugins: [
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         }),
-        new MinifyPlugin()
+        new UglifyJsPlugin({
+            uglifyOptions: {
+                ecma: 8,
+                parallel: true,
+                output: {
+                    comments: false,
+                    beautify: false
+                },
+                safari10: true,
+            }
+        }),
+        new CompressionPlugin({
+          asset: "[path].gz[query]",
+          algorithm: "gzip",
+          test: /\.js$|\.css$|\.html$/,
+          threshold: 10240,
+          minRatio: 0.8
+        })
     ]
 });
