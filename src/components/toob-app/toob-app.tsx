4c412a89a4b2c1f4c1f15308272380c46d6a500d
Niklas Kühtmann <admin@i-zed.net>
2018-04-26 23:45:00 +0200

    Added header. Added styling of app. Updated toob-app.

--- src/components/toob-app/toob-app.tsx
+++ src/components/toob-app/toob-app.tsx
@@ -1,6 +1,7 @@
 import '@stencil/core';
 import { Component, State, Listen } from '@stencil/core';
 import { SearchResult } from "../toob-search/toob-search";
+import { ToobVideo, VideoStatus } from "../toob-video/toob-video";
 
 interface AppSettings {
     autodownload: boolean;
@@ -28,6 +29,15 @@ export class ToobApp {
         this.entries = [...this.entries, result]
     }
 
+    @Listen("statusUpdate")
+    handleStatusUpdate({video, status}: {video: ToobVideo, status: VideoStatus}) {
+        switch (status) {
+            case VideoStatus.added: this.settings.autodownload && video.download(); break;
+            case VideoStatus.downloaded: this.settings.autoconvert && video.convert(); break;
+            default: break;
+        }
+    }
+
     changeSetting<T extends keyof AppSettings>(key: T) {
         return (checked: boolean) => {
             const settings = Object.assign({}, this.settings);
@@ -38,29 +48,33 @@ export class ToobApp {
     }
 
 	render() {
-		return (
-            <div class="container">
+        const videoentries: JSX.Element[] = []
+		return [
+            <div class="background" />,
+            <div class="body">
                 <div class="background" />
-                <div class="body">
-                    <div class="background" />
-                    <div class="header">
-                        <div class="icon" />
-                        <div class="title" >toob.host</div>
-                    </div>
-                    <toob-search />
-                    <div class="settings">
-                        <toob-checkbox checked={this.settings.autoconvert} onChanged={this.changeSetting("autoconvert")}>
-                            Auto-Convert
-                        </toob-checkbox>
-                        <toob-checkbox checked={this.settings.autodownload} onChanged={this.changeSetting("autodownload")}>
-                            Auto-Download
-                        </toob-checkbox>
-                    </div>
+                <div class="header">
+                    <div class="icon" />
+                    <div class="title" >toob.host</div>
                 </div>
-                <div class="footer">
-                    &copy; 2017-2018 <a href="https://ized.io/">ized.io</a>
+                <toob-search />
+                <div class="settings">
+                    <toob-checkbox checked={this.settings.autoconvert} onChanged={this.changeSetting("autoconvert")}>
+                        Auto-Convert
+                    </toob-checkbox>
+                    <toob-checkbox checked={this.settings.autodownload} onChanged={this.changeSetting("autodownload")}>
+                        Auto-Download
+                    </toob-checkbox>
                 </div>
+                <toob-list>
+                    { this.entries.map(entry => (
+                        <toob-video data={entry} />
+                    )) }
+                </toob-list>
+            </div>,
+            <div class="footer">
+                &copy; 2017-2018 <a href="https://ized.io/">ized.io</a>
             </div>
-		);
+        ];
 	}
 }
